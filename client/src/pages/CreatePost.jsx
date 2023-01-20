c98929ae93a1c18c66b6d3d73c0157a8216c1f09
Sahitya77 <sahityanijhawan@gmail.com>
2023-01-21 01:25:30 +0530

    new page added

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+import { preview } from '../assets'
+import { FormField,Loader } from '../components'
+import {getRandomPrompt} from '../utils';
 const CreatePost = () => {
+  const navigate=useNavigate();
+  const [form, setForm] = useState({
+    name:'',
+    prompt:'',
+    photo:'',
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1  className='font-extrabold text-[#222328] text-[32px]'>
+          Create
+        </h1>
+        <p className='mt-2 text-[#666e75] text-[18px] max-w-[500px] '>
+          Create imaginative and visually stunning images.
+        </p>
+      </div>
+      <form className='mt-16 max-w-3xl'>
+
+      </form>
+
+    </section>
   )
 }
 
