b7aa82bbd81ac8bc4f7d3c1963af60d491ffc44e
Sahitya77 <sahityanijhawan@gmail.com>
2023-01-28 16:21:17 +0530

    backend routes changes

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -17,7 +17,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://imagegenerator-f3vt.onrender.com//api/v1/dalle', {
+        const response = await fetch('https://imagegenerator-f3vt.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
